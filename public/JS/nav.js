40ed51d40674ad84bd509f2698552fb56470ead0
PhilFerrand <splendira@outlook.com>
2024-10-26 10:34:59 +0200

    nav test

---
+++ public/JS/nav.js
@@ -0,0 +1,36 @@
+const navContainer = document.querySelector(".nav");
+
+function createMenu() {
+  const nav = document.createElement("nav");
+
+  nav.innerHTML = `
+  
+  <div class="left">
+    <a href="/"><img src="../public/img/logo.png" alt="" /></a>
+  </div>
+  <div class="right">
+    <a href="/pages/club.html" aria-label="Club page">Le club</a>
+    <a href="#" aria-label="Information sur le club">Informations</a>
+    <a href="#" aria-label="Les photos du club">Diaporama</a>
+    <a href="#" aria-label="Nous contacter">Contact</a>
+    <a href="#" aria-label="Formulaire d'inscription">Inscription</a>
+  </div>
+
+  <div class="hamburger-icone">
+    <i class="bx bx-menu"></i>
+  </div>
+  `;
+
+  navContainer.append(nav);
+
+  const menu = document.querySelector(".right");
+  const iconeHamburger = document.querySelector(".hamburger-icone");
+
+  iconeHamburger.addEventListener("click", activeMenu);
+
+  function activeMenu() {
+    menu.classList.toggle("active-menu");
+  }
+}
+
+createMenu();
